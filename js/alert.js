eb329241e094353a6fedf62b4fba02e4859b4bb6
yamauchitomoya <42059799+plus-agc-service-yamauchi@users.noreply.github.com>
2020-12-14 09:52:00 +0900

    20201214
    update

---
+++ js/alert.js
@@ -0,0 +1,4 @@
+var userAgent = window.navigator.userAgent;
+    if(ua.indexOf('Trident') != -1 || ua.indexOf('mSIE') != -1){
+        alert('お使いのブラウザは閲覧推奨環境ではありません。\nウェブサイトが正しく表示されない、動作しない等の現象が起こる場合がありますので予めご了承ください。\nChromeまたはEdgeブラウザのご利用をおすすめいたします。');
+    }
