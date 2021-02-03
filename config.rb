d5a5ee95c4a35df37766ea98db7f92ccc8db756e
yamauchitomoya <42059799+plus-agc-service-yamauchi@users.noreply.github.com>
2021-02-03 09:47:22 +0900

    20210203
    update

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+output_style = "expanded"
+line_comments = false
+relative_assets = true
+Encoding.default_external = "utf-8"
